895ab4ec4e3f7af11cd75e33c26773feadae2c49
Николай <kolia047@mail.ru>
2022-09-05 08:51:47 +0600

    1. Ввести с клавиатуры число N. 2. Считать N целых чисел и заполнить ими список - метод getIntegerList. 3. Найти минимальное число среди элементов списка - метод getMinimum

--- src/lessons11/Exampl6.java
+++ src/lessons11/Exampl6.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Exampl6 {
@@ -27,12 +29,6 @@ public class Exampl6 {
     }
 
     static int getMinimum(List<Integer> list) {
-        int min = list.get(0);
-        for (int n : list) {
-            if (min > n) {
-                min = n;
-            }
-        }
-        return min;
+        return Collections.min(list);
     }
 }
